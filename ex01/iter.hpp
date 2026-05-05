1efc4fd72e8a4db0b714a693f4558875c32efe4f
Khaldoun Alhanawi <khaldounalhanawi@users.noreply.github.com>
2026-05-05 21:10:12 +0200

    ex01:done

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -1,5 +1,20 @@
 #ifndef ITER_HPP
 # define ITER_HPP
 
+#include <cstddef>
+
+template <typename T, typename Y>
+void	iter(T *array, const size_t len, Y fn)
+{
+	for (size_t i = 0; i < len; i ++)
+		fn(array[i]);
+}
+
+template <typename T, typename Y>
+void	iter(const T *array, const size_t len, Y fn)
+{
+	for (size_t i = 0; i < len; i ++)
+		fn(array[i]);
+}
 
 #endif
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,7 +1,38 @@
 #include "iter.hpp"
+#include <iostream>
+
+void	hey(int &i)
+{
+	std::cout << i << std::endl;
+	i = 0;
+}
+
+void	heyChar(char &c)
+{
+	std::cout << c << std::endl;
+	c = 'F';
+}
 
 int	main()
 {
-	
+	{std::cout << ">>1<<\n";
+
+	int arr[4] = {3,2,1,5};
+
+	::iter (arr, 4, hey);
+
+	std::cout << "**************************\n";
+	for (int i = 0; i < 4; i ++)
+		std::cout << arr[i] << "\n";
+	}
+	{std::cout << ">>2<<\n";
+	char line[3] = {'a', 'b', 'c'};
+
+	::iter (line, 4, heyChar);
+
+	std::cout << "**************************\n";
+	for (int i = 0; i < 3; i ++)
+		std::cout << line[i] << "\n";
+	}
 	return (0);
 }
